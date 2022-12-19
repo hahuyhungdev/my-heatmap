02bc850ac0fd0931e48a9736ac995299c8641f5e
hà huy hùng <83862705+hahuyhungdev@users.noreply.github.com>
2022-12-19 14:47:52 +0700

    update tooltip

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import h337 from "heatmap.js";
-import React, { useEffect, useMemo, useRef } from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 import mqtt from "precompiled-mqtt";
 
 import "./style.css";
 
 function App() {
-  const [data, setData] = React.useState([]);
+  const [dataApp, setDataApp] = React.useState([]);
+
+  const [typeGetValue, setTypeGetValue] = useState(false);
   const testData = useMemo(() => [], []);
   const points = useRef({});
   const client = mqtt.connect("wss://test.mosquitto.org:8081");
@@ -19,41 +21,79 @@ function App() {
   //     console.log(message.toString());
   //   });
   // });
+  // handle activeGetvalue button get value
+  console.log(typeGetValue === false ? "Get value" : "Stop get value");
+  const handleActiveGetValue = () => {
+    setTypeGetValue(!typeGetValue);
+  };
   useEffect(() => {
     var heatmapInstance = h337.create({
-      container: document.querySelector(".demo-wrapper"),
-      radius: 10,
+      // container: document.querySelector(".demo-wrapper"),
+      container: document.querySelector(typeGetValue === false ? ".demo-wrapper" : ".heatmap"),
+      radius: 20,
       maxOpacity: 2.5,
       minOpacity: 0,
       blur: 0.75,
       height: 500,
     });
-    // const logs = [];
-    var dataLocal = localStorage.getItem("testData");
-    dataLocal = JSON.parse(dataLocal);
-    document.querySelector(".demo-wrapper").onmousemove = function (ev) {
-      points.current = {
-        x: ev.layerX,
-        y: ev.layerY,
-        value: Math.random() * 100,
-      };
-      // console.log(points.current);
-      testData.push(points.current);
-      setData(testData);
-      // save test data to local storage limit 100
-      if (testData.length < 100) {
-        localStorage.setItem("testData", JSON.stringify(testData));
-      }
-      if (points.current !== null && points.current !== undefined) {
-        client.publish("dataMqtt", JSON.stringify(points.current));
-        heatmapInstance.addData(points.current === null ? dataLocal : points.current);
+    var demoWrapper = document.querySelector(".demo-wrapper");
+    var tooltip = document.querySelector(".tooltip");
+    var dataLocal = JSON.parse(localStorage.getItem("testData"));
+    var max = 0;
+    var val = Math.floor(Math.random() * 100);
+    function updateTooltip(x, y, value) {
+      // + 15 for distance to cursor
+      var transform = "translate(" + (x + 15) + "px, " + (y + 15) + "px)";
+      tooltip.style.MozTransform = transform; /* Firefox */
+      tooltip.style.msTransform = transform; /* IE (9+) - note ms is lowercase */
+      tooltip.style.OTransform = transform; /* Opera */
+      tooltip.style.WebkitTransform = transform; /* Safari and Chrome */
+      tooltip.style.transform = transform; /* One day, my pretty */
+      tooltip.innerHTML = value;
+    }
+
+    demoWrapper.onmousemove = function (ev) {
+      if (typeGetValue === false) {
+        points.current = {
+          x: ev.layerX,
+
+          y: ev.layerY,
+          value: Math.floor(Math.random() * 100),
+        };
+        // console.log(points.current);
+        testData.push(points.current);
+        setDataApp(testData);
+
+        // save test data to local storage limit 100
+        if (testData.length < 500) {
+          localStorage.setItem("testData", JSON.stringify(testData));
+        }
+        if (points.current !== null && points.current !== undefined) {
+          client.publish("dataMqtt", JSON.stringify(points.current));
+          heatmapInstance.addData(points.current === null || typeGetValue ? dataLocal : points.current);
+          // heatmapInstance.addData(dataLocal);
+        }
+      } else {
+        var x = ev.layerX;
+        var y = ev.layerY;
+        var value = heatmapInstance.getValueAt({
+          x: x,
+          y: y,
+        });
+
+        updateTooltip(x, y, value);
+        tooltip.style.display = "block";
       }
     };
-
-    heatmapInstance.addData(dataLocal === null ? data : dataLocal);
-
-    // heatmapInstance.addData(points.current);
-  }, [data, testData, client]);
+    demoWrapper.onmouseout = function () {
+      tooltip.style.display = "none";
+    };
+    console.log(dataApp);
+    heatmapInstance.setData({
+      max: Math.max(max, val),
+      data: dataLocal === null ? dataApp : dataLocal,
+    });
+  }, [dataApp, testData, client, typeGetValue]);
   client.subscribe("dataMqtt");
   client.on("message", (topic, message) => {
     console.log("message", message.toString());
@@ -68,25 +108,29 @@ function App() {
       <h2>Start editing to see some magic happen!</h2>
       <div className="demo-wrapper">
         <div className="heatmap"></div>
+        <div className="tooltip"></div>
       </div>
-      <button
+      <div
+        className="function"
         style={{
-          height: "30px",
-          borderRadius: "10px",
-          backgroundColor: "#f44336",
           display: "flex",
-          cursor: "pointer",
-          margin: "0 auto 20px",
-          padding: "10px",
+          gap: "10px",
+          justifyContent: "center",
           alignItems: "center",
         }}
-        className="resetData"
-        onClick={() => {
-          handleResetData();
-        }}
       >
-        Reset Data
-      </button>
+        <button
+          className="resetData"
+          onClick={() => {
+            handleResetData();
+          }}
+        >
+          Reset Data
+        </button>
+        <button className="activeGetvalue" onClick={handleActiveGetValue}>
+          {typeGetValue === false ? "Get value" : "Stop get value"}
+        </button>
+      </div>
     </div>
   );
 }
